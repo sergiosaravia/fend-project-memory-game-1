2fbcfdbb3d930ef23f561f1bc2b819d8733bab07
Sergio Saravia <sergiosaravia74@gmail.com>
2018-06-10 17:56:26 -0400

    remove and add li
    remove li, add li with js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 /*
  * Create a list that holds all of your cards
  */
+// Creates card deck array //
+const cardDeck = [
+      [1, 'fa-diamond'],
+      [1, 'fa-diamond'],
+      [2, 'fa-paper-plane-o'],
+      [2, 'fa-paper-plane-o'],
+      [3, 'fa-bomb'],
+      [3, 'fa-bomb'],
+      [4, 'fa-anchor'],
+      [4, 'fa-anchor'],
+      [5, 'fa-leaf'],
+      [5, 'fa-leaf'],
+      [6, 'fa-bicycle'],
+      [6, 'fa-bicycle'],
+      [7, 'fa-cube'],
+      [7, 'fa-cube'],
+      [8, 'fa-bicycle'],
+      [8, 'fa-bicycle']
+   ];
+
+
+// Global variables
+
 
 
 /*
@@ -36,3 +59,19 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+let gameCanvas = "";
+
+ function setGameBoard() {
+   let gameCanvas = document.getElementById("deck");
+       cardDeck.shuffle();
+       for (i = 0; i < cardDeck.length; i++) {
+          gameCanvas += '<ul class="deck' + cardDeck[i][0] + '"><i class="fas ' + cardDeck[i][1] + '"></i></ul>';
+       }
+
+
+
+// Reset canvas
+
+gameBoard.innerHTML = gameCanvas;
+
+setGameBoard();
